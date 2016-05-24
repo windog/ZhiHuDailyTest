1111aa05d8b0c51e6a9ecccfab0a1fbcd8d6bbd7
windog <windiscall@gmail.com>
2016-05-24 20:18:16 +0800

    Initial commit

---
+++ app/src/main/java/com/windy/example/zhihudailytest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.windy.example.zhihudailytest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
